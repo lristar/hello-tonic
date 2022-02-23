ee1531e2dea39d9cff5b68d0a7c007d31526124f
Lizhuyou <84826607@qq.com>
2022-02-23 15:45:02 +0800

    init client

---
+++ main/src/client.rs
@@ -0,0 +1,23 @@
+// use hello_world::greeter_client::GreeterClient;
+// use hello_world::HelloRequest;
+
+// pub mod hello_world {
+//     tonic::include_proto!("helloworld");
+// }
+use api::helloworld::greeter_client::GreeterClient;
+use api::helloworld::HelloRequest;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(HelloRequest {
+        name: "Tonic".into(),
+    });
+
+    let response = client.say_hello(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
